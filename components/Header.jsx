c85282b2d0866ad2055ca3a96b681e38b5553d85
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-04-13 13:11:17 -0300

    Header e Footer

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <footer>
+                <div>
+                    <h3>Header</h3>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Header;
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function Home() {
+  return <div>
+    <Header></Header>
+    <Footer></Footer>
+  </div>;
+}
